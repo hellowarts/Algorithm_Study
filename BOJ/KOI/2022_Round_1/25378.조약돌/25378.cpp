ecea4c912fe7ff1532f08951ae54693323b2f36e
KimTaeho <xoghrla0311@gmail.com>
2025-02-17 15:48:32 +0900

    정올 조약돌 문제 풀이

--- BOJ/KOI/2022_Round_1/25378.조약돌/25378.cpp
+++ BOJ/KOI/2022_Round_1/25378.조약돌/25378.cpp
@@ -1,5 +1,32 @@
 #include<bits/stdc++.h>
 
+static int N;
+static int arr[2501];
+int DP[2501];
+
 int main(void) {
+
+    std::cin >> N;
+    for(int i = 1; i<=N; i++) 
+        std::cin >> arr[i];
+    
+    for(int i=2; i<=N; i++) {
+        // 1번 작업을 적용할 수 있는 수
+        int remain = DP[i-1];
+        int x = arr[i];
+        for(int j=i-1; j; j--) {
+            x = arr[j] - x;
+            // 1번 쟉업을 수행할 때 값이 음수가 되어선 안된다.
+            if(x < 0) break;
+            // 1번 작업을 적용할 수 있는 경우 (인접한 두 수가 같은 경우) DP 업데이트
+            if(!x && remain <= DP[j-1])
+                remain = DP[j-1] + 1; 
+        }
+        DP[i] = remain;
+    }
+
+    // 최대 횟수 N번에서 1번을 적용할 수 있는 횟수를 빼면 답이 된다.
+    std::cout << N - DP[N];
+
     return 0;
 }
